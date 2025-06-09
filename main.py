30b37d7120f906a8677566cf8cecd49450bc3e58
Anirban Saha <76745087+SahaAnirban007@users.noreply.github.com>
2025-06-09 21:50:36 +0530

    title change, readability, reset button

--- main.py
+++ main.py
@@ -3,51 +3,58 @@ from flask import Flask, render_template, request, redirect, url_for, session
 app = Flask(__name__)
 app.secret_key = 'secret'  # Needed for session
 
-def checkWin(xState, zState):
+def win_check(x_position, z_position):
     wins = [[0, 1, 2], [3, 4, 5], [6, 7, 8],
             [0, 3, 6], [1, 4, 7], [2, 5, 8],
             [0, 4, 8], [2, 4, 6]]
     for win in wins:
-        if sum([xState[i] for i in win]) == 3:
-            return 'X'
-        if sum([zState[i] for i in win]) == 3:
-            return 'O'
+        if sum([x_position[i] for i in win]) == 3:
+            return 'player_O'
+        if sum([z_position[i] for i in win]) == 3:
+            return 'player_o'
     return None
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    if 'xState' not in session:
-        session['xState'] = [0]*9
-        session['zState'] = [0]*9
+    if 'x_position' not in session:
+        session['x_position'] = [0]*9
+        session['z_position'] = [0]*9
         session['turn'] = 1
         session['winner'] = None
+        session['player_x'] = ''
+        session['player_o'] = ''
 
-    return render_template("index.html",
-                           xState=session['xState'],
-                           zState=session['zState'],
-                           turn=session['turn'],
-                           winner=session['winner'])
+    if request.method == 'POST':
+        if 'player_x' in request.form and 'player_o' in request.form:
+            # Save player names
+            session['player_x'] = request.form['player_x']
+            session['player_o'] = request.form['player_o']
+        elif 'cell' in request.form and not session.get('winner'):
+            # Handle move
+            cell = int(request.form['cell'])
+            x_position = session['x_position']
+            z_position = session['z_position']
 
-@app.route('/play/<int:cell>')
-def play(cell):
-    if session.get('winner'):
+            if x_position[cell] == 0 and z_position[cell] == 0:
+                if session['turn'] == 1:
+                    x_position[cell] = 1
+                else:
+                    z_position[cell] = 1
+
+                session['winner'] = win_check(x_position, z_position)
+                session['turn'] = 1 - session['turn']
+                session['x_position'] = x_position
+                session['z_position'] = z_position
+
         return redirect(url_for('index'))
 
-    xState = session['xState']
-    zState = session['zState']
-    turn = session['turn']
-
-    if xState[cell] == 0 and zState[cell] == 0:
-        if turn == 1:
-            xState[cell] = 1
-        else:
-            zState[cell] = 1
-        winner = checkWin(xState, zState)
-        session['xState'] = xState
-        session['zState'] = zState
-        session['turn'] = 1 - turn
-        session['winner'] = winner
-    return redirect(url_for('index'))
+    return render_template("index.html",
+                           x_position=session['x_position'],
+                           z_position=session['z_position'],
+                           turn=session['turn'],
+                           winner=session['winner'],
+                           player_x=session.get('player_x', ''),
+                           player_o=session.get('player_o', ''))
 
 @app.route('/reset')
 def reset():
